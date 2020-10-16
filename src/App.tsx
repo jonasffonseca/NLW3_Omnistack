cab489b01e8912503299c2a12dd14e8edb51260f
Jonas Fonseca <jonas.fragafonseca@gmail.commit>
2020-10-15 23:42:44 -0300

    Omnistack Frontend Ready

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
+import './pages/Landing';
+import Routes from './routes';
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
